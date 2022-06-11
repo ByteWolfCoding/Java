3d1eb71c5897c22fbc848fa2fbdac75adfa8b791
FabianBaitura <85544073+FabianBaitura@users.noreply.github.com>
2022-06-11 22:01:37 +0200

    trying out Javas Print Function for the first time

---
+++ Programs/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("This is my first Java Code!");
+
+        print("This print looks like python!\n");
+        print("Java is my 5th languege!");
+    }
+    private static void print(String text){
+        System.out.println(text);
+    }
+}
